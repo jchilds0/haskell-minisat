f8b96385b1940467910200e8d1c00c2a369ff1cd
Josh <joshua.childs01@outlook.com>
2024-06-24 15:03:28 +1000

    updated benchmarks

--- src/Bench/Main.hs
+++ src/Bench/Main.hs
@@ -4,53 +4,104 @@ module Main where
 
 import Data.Time.Clock
 import MiniSat.Solver
+import Numeric
 import MiniSat.Parser
+import System.Directory
+import Control.Concurrent.PooledIO.Independent
+import GHC.GHCi.Helpers (flushAll)
 
-dir = "./src/Bench/unfsat/"
+dir :: String
+dir = "./benchmarks/"
+
+numTests :: Int
+numTests = 1000
+
+testSets :: [(String, Bool)]
+testSets = [
+        -- Uniform Random
+        ("uf20-91/", True), 
+        ("uf50-218/", True), 
+        ("uf75-325/", True),
+        ("uf100-430/", True), 
+        ("uf125-538/", True),
+        ("uf150-645/", True), 
+        ("uf175-753/", True),
+        ("uf200-860/", True), 
+        ("uf225-960/", True),
+        ("uf250-1065/", True),
+        
+        ("uuf50-218/", False),
+        ("uuf75-325/", False),
+        ("uuf100-430/", False),
+        ("uuf125-538/", False),
+        ("uuf150-645/", False),
+        ("uuf175-753/", False),
+        ("uuf200-860/", False),
+        ("uuf225-960/", False),
+        ("uuf250-1065/", False)
+
+        -- Random + Backbone minimal
+        -- ("RTI_k3_n100_m429/", True),
+        -- ("BMS_k3_n100_m429/", True),
+
+        -- Random + Controlled backbone
+        -- ("CBS_k3_n100_m403_b10/", True),
+        -- ("CBS_k3_n100_m403_b30/", True),
+        -- ("CBS_k3_n100_m403_b50/", True)
+    ]
 
 main :: IO ()
 main = do
     -- testFile "dubois22.cnf"
+    let files = map fst testSets
+    let times = map (\(f, sat) -> testFiles (dir ++ f) sat) testSets
+    let results = zipWith printResult times files
+    sequence_ results
 
-    putStrLn "--- 20 Variables ---"
-    let file1 = map (\x -> "uf20-0" ++ show x ++ ".cnf") [1..10]
-    mapM_ testFile file1
+testFiles :: String -> Bool -> IO Double
+testFiles path sat = do
+    names <- getDirectoryContents path
+    let fileNames = filter (\n -> n `notElem` [".", ".."]) names
+    let files = map (path ++) (take numTests fileNames)
+    times <- mapM (testFile sat) files
 
-    putStrLn "--- 50 Variables ---"
-    let file2 = map (\x -> "uf50-0" ++ show x ++ ".cnf") [1..10]
-    mapM_ testFile file2
+    return (avg times)
 
-    putStrLn "--- 75 Variables ---"
-    let file3 = map (\x -> "uf75-0" ++ show x ++ ".cnf") [1..10]
-    mapM_ testFile file3
+avg :: [Double] -> Double
+avg ls = sum ls / fromIntegral (length ls)
 
-    putStrLn "--- 100 Variables ---"
-    let file3 = map (\x -> "uf100-0" ++ show x ++ ".cnf") [1..10]
-    mapM_ testFile file3
-
-    -- putStrLn "--- 150 Variables ---"
-    -- let file3 = map (\x -> "uf150-0" ++ show x ++ ".cnf") [1..10]
-    -- mapM_ testFile file3
-
-
-testFile :: String -> IO ()
-testFile fileName = do
-    rows <- readFile (dir ++ fileName)
+testFile :: Bool -> String -> IO Double
+testFile sat file = do
+    rows <- readFile file
     let ls = lines rows
     let model = dimacsToModel ls
-
-    putStr $ "\t" ++ fileName ++ ": "
 
     start <- getCurrentTime
     let sol = solve model
     case sol of 
-        Just ls -> do
-            putStr "Solution   "
-        Nothing -> 
-            putStr "No Solution"
+        Just _ -> if not sat then putStrLn $ file ++ " is not sat" else putStr ""
+        Nothing -> if sat then putStrLn $ file ++ " is sat" else putStr ""
 
     end <- getCurrentTime
-    putStrLn $ "\t(Took: " ++ show (diffUTCTime end start) ++ ")"
+    let timeDiff = diffUTCTime end start
+    return (realToFrac timeDiff)
+
+
+printResult :: IO Double -> String -> IO ()
+printResult t file = do 
+    putStr $ file ++ "\t" 
+    time <- t
+    printTime time
+    putStrLn ""
+
+printTime :: Double -> IO ()
+printTime timeS = do 
+    let timeMS = 1000 * timeS
+    if timeS < 1 then
+        putStr $ showFFloat (Just 2) timeMS "" ++ " ms"
+    else 
+        putStr $ showFFloat (Just 2) timeS "" ++ " s"
+
 
 printList :: Show a => [a] -> IO ()
 printList [] = putStrLn ""
